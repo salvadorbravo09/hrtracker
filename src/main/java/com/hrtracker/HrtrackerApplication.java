0485cd7c67a0fef32b30aa694c21f7da339e5a2e
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-16 17:20:20 -0300

    initial commit

---
+++ src/main/java/com/hrtracker/HrtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.hrtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrtracker/HrtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
