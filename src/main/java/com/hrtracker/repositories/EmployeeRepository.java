ab6963a257d5d41decc0c2209b2fe8df3bc76648
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-16 18:37:01 -0300

    feature: add jpa repositories

---
+++ src/main/java/com/hrtracker/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.hrtracker.repositories;
+
+import com.hrtracker.models.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
