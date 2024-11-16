d91aed59b33a08b9c86a31bffefd1b9b905d43bf
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-16 19:03:40 -0300

    feat: add service annotation

--- src/main/java/com/hrtracker/services/impl/DepartmentServiceImpl.java
+++ src/main/java/com/hrtracker/services/impl/DepartmentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.hrtracker.services.impl;
 
 import com.hrtracker.services.DepartmentService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DepartmentServiceImpl implements DepartmentService {
 }
--- src/main/java/com/hrtracker/services/impl/EmployeeServiceImpl.java
+++ src/main/java/com/hrtracker/services/impl/EmployeeServiceImpl.java
@@ -1,6 +1,8 @@
 package com.hrtracker.services.impl;
 
 import com.hrtracker.services.EmployeeService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 }
