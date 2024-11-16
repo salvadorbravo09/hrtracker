4fee9b8da8fcc9fe3e4c07402323143ed0db47ca
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-16 19:21:37 -0300

    feat: create employee service interface and its implementation

--- src/main/java/com/hrtracker/services/EmployeeService.java
+++ src/main/java/com/hrtracker/services/EmployeeService.java
@@ -1,4 +1,12 @@
 package com.hrtracker.services;
 
+import com.hrtracker.models.entities.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+    Employee getEmployeeById(Long id);
+    Employee createEmployee(Employee employee);
 }
--- src/main/java/com/hrtracker/services/impl/EmployeeServiceImpl.java
+++ src/main/java/com/hrtracker/services/impl/EmployeeServiceImpl.java
@@ -1,8 +1,35 @@
 package com.hrtracker.services.impl;
 
+import com.hrtracker.models.entities.Employee;
+import com.hrtracker.repositories.EmployeeRepository;
 import com.hrtracker.services.EmployeeService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(Long id) {
+        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+        return optionalEmployee.orElseThrow();
+    }
+
+    @Override
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 }
