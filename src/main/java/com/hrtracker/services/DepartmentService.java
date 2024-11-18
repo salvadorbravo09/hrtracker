001d4b0c3a3a877b04ad106fd345675cba48fc22
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-18 18:11:04 -0300

    feat: add DepartmentService interface with methods

--- src/main/java/com/hrtracker/services/DepartmentService.java
+++ src/main/java/com/hrtracker/services/DepartmentService.java
@@ -1,4 +1,15 @@
 package com.hrtracker.services;
 
+import com.hrtracker.models.entities.Department;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface DepartmentService {
+
+    List<Department> getAllDepartment();
+    Optional<Department> getDepartmentById(Long id);
+    Department createDepartment(Department department);
+    Department updateDepartment(Long id, Department department);
+    void deleteDepartment(Long id);
 }
