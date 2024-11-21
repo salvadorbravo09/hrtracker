47860d417262509d6a66a681c5f15b2a6e18dd95
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-20 22:18:27 -0300

    refactor: validation rules were added to the entity for the db

--- src/main/java/com/hrtracker/models/dtos/DepartmentCreateDto.java
+++ src/main/java/com/hrtracker/models/dtos/DepartmentCreateDto.java
@@ -1,5 +1,7 @@
 package com.hrtracker.models.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,5 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class DepartmentCreateDto {
 
+    @NotBlank(message = "Name is required")
+    @Size(max = 50, message = "The name must be not exceed 50 characters")
     private String name;
 }
--- src/main/java/com/hrtracker/models/dtos/DepartmentSimpleDto.java
+++ src/main/java/com/hrtracker/models/dtos/DepartmentSimpleDto.java
@@ -1,5 +1,7 @@
 package com.hrtracker.models.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +12,8 @@ import lombok.NoArgsConstructor;
 public class DepartmentSimpleDto {
 
     private Long id;
+
+    @NotBlank
+    @Size(min = 50, message = "The name must not exceed 50 characters")
     private String name;
 }
