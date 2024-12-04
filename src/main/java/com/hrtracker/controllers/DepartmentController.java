5d43dbc3764d206bab30db5cce8695ba35a1c4ac
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-04 18:42:54 -0300

    feat: impl PreAuthorize with roles in DepartmentController

--- src/main/java/com/hrtracker/controllers/DepartmentController.java
+++ src/main/java/com/hrtracker/controllers/DepartmentController.java
@@ -1,14 +1,13 @@
 package com.hrtracker.controllers;
 
 import com.hrtracker.models.dtos.DepartmentCreateDto;
-import com.hrtracker.models.dtos.DepartmentDto;
 import com.hrtracker.models.dtos.DepartmentSimpleDto;
-import com.hrtracker.models.entities.Department;
 import com.hrtracker.services.DepartmentService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,11 +17,13 @@ import java.util.Optional;
 @RequestMapping("/api/v1/department")
 @RequiredArgsConstructor
 @Tag(name = "Department", description = "Operations related to departments")
+@PreAuthorize("authenticated()")
 public class DepartmentController {
 
     private final DepartmentService departmentService;
 
     @GetMapping
+    @PreAuthorize("permitAll()")
     public ResponseEntity<List<DepartmentSimpleDto>> findAllDepartments() {
         List<DepartmentSimpleDto> departments = departmentService.getAllDepartment();
         if (departments.isEmpty()) {
@@ -32,6 +33,7 @@ public class DepartmentController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('READ')")
     public ResponseEntity<DepartmentSimpleDto> findDepartmentById(@PathVariable Long id) {
         Optional<DepartmentSimpleDto> departmentSimpleDto = departmentService.getDepartmentById(id);
         return departmentSimpleDto
@@ -40,18 +42,21 @@ public class DepartmentController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('CREATE')")
     public ResponseEntity<DepartmentSimpleDto> createDepartment(@RequestBody DepartmentCreateDto department) {
         DepartmentSimpleDto newDepartment = departmentService.createDepartment(department);
         return ResponseEntity.status(HttpStatus.CREATED).body(newDepartment);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('UPDATE')")
     public ResponseEntity<DepartmentSimpleDto> updateDepartment(@PathVariable Long id, @RequestBody DepartmentCreateDto department) {
         DepartmentSimpleDto updatedDepartment = departmentService.updateDepartment(id, department);
         return ResponseEntity.status(HttpStatus.OK).body(updatedDepartment);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('DELETE')")
     public ResponseEntity<?> deleteDepartment(@PathVariable Long id) {
         departmentService.deleteDepartment(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
